48c1694386e8278e4dcaea9d6f9101aa39452da7
Nab244 <nrizki@simplonformations.onmicrosoft.com>
2023-07-03 13:15:28 +0200

    firstfiles

---
+++ network.tf
@@ -0,0 +1,73 @@
+data "azurerm_resource_group" "rg" {
+  name = "Nabila_R"
+}
+
+locals {
+  ipSpace    = ["10.1.0.0/16"]
+  prefixName = "nab-"
+  subnets = {
+    "${local.prefixName}subnet-VM"      = "10.1.1.0/24"
+    "${local.prefixName}subnet-3"       = "10.1.2.0/24"
+  }
+}
+
+# Create virtual network
+resource "azurerm_virtual_network" "Vnet" {
+  name                = "${local.prefixName}vn-virtual-network"
+  address_space       = local.ipSpace
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+# Create subnet
+resource "azurerm_subnet" "Subnet" {
+  for_each             = local.subnets
+  name                 = each.key
+  address_prefixes     = [each.value]
+  resource_group_name  = data.azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.Vnet.name
+}
+
+# Create Network Security Group and rule for Bastion
+resource "azurerm_network_security_group" "nsgBastion" {
+  name                = "${local.prefixName}nsgBastion"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "${data.external.adminipaddr.result.ip}"
+    destination_address_prefix = "*"
+  }
+}
+
+#Get adminuser public IP 
+data "external" "adminipaddr" {
+  program = ["bash", "-c", "curl -s 'https://ipinfo.io/json'"]
+}
+
+# Create Network Security Group and rule for App
+resource "azurerm_network_security_group" "nsgApp" {
+  name                = "${local.prefixName}nsgApp"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
---
+++ virtualmachines.tf
@@ -0,0 +1,117 @@
+# Create Bastion public IP
+resource "azurerm_public_ip" "ipBastion" {
+  name                         = "${local.prefixName}ip-Bastion"
+  location                     = data.azurerm_resource_group.rg.location
+  resource_group_name          = data.azurerm_resource_group.rg.name
+  allocation_method = "Static"
+  domain_name_label            = "${local.prefixName}bastion"
+}
+
+#Create Bastion nic
+resource "azurerm_network_interface" "nicBastion" {
+  name                    = "${local.prefixName}nic-bastion"
+  location                = data.azurerm_resource_group.rg.location
+  resource_group_name     = data.azurerm_resource_group.rg.name
+  internal_dns_name_label = "bastion"
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.Subnet["${local.prefixName}subnet-VM"].id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id   = azurerm_public_ip.ipBastion.id
+  }
+}
+
+#Association of nic and nsg for Bastion VM
+resource "azurerm_network_interface_security_group_association" "nicnsgbastion" {
+  network_interface_id      = azurerm_network_interface.nicBastion.id
+  network_security_group_id = azurerm_network_security_group.nsgBastion.id
+}
+
+
+# Create bastion virtual machine
+resource "azurerm_linux_virtual_machine" "bastion" {
+  name                  = "${local.prefixName}vm-bastion"
+  location              = data.azurerm_resource_group.rg.location
+  resource_group_name   = data.azurerm_resource_group.rg.name
+  admin_username = "nabila"
+  size                  = "Standard_DS1_v2"
+  network_interface_ids = [azurerm_network_interface.nicBastion.id]
+
+  os_disk {
+    name                 = "${local.prefixName}OsDisk-bastion"
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+  admin_ssh_key {
+    username   = "nabila"
+    public_key = file("~/.ssh/terraform_key.pub")
+  }
+}
+
+
+
+# Create App public IP
+resource "azurerm_public_ip" "ipApp" {
+  name                         = "${local.prefixName}ip-App"
+  location                     = data.azurerm_resource_group.rg.location
+  resource_group_name          = data.azurerm_resource_group.rg.name
+  allocation_method = "Static"
+  domain_name_label            = "${local.prefixName}app"
+}
+
+#Create App nic
+resource "azurerm_network_interface" "nicApp" {
+  name                    = "${local.prefixName}nic-app"
+  location                = data.azurerm_resource_group.rg.location
+  resource_group_name     = data.azurerm_resource_group.rg.name
+  internal_dns_name_label = "app"
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.Subnet["${local.prefixName}subnet-VM"].id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id    = azurerm_public_ip.ipApp.id
+  }
+}
+
+#Association of nic and nsg for App VM
+resource "azurerm_network_interface_security_group_association" "nicnsgapp" {
+  network_interface_id      = azurerm_network_interface.nicApp.id
+  network_security_group_id = azurerm_network_security_group.nsgApp.id
+}
+
+
+# Create app virtual machine
+resource "azurerm_linux_virtual_machine" "app" {
+  name                  = "${local.prefixName}vm-app"
+  location              = data.azurerm_resource_group.rg.location
+  resource_group_name   = data.azurerm_resource_group.rg.name
+  admin_username = "nabila"
+  size                  = "Standard_DS1_v2"
+  network_interface_ids = [azurerm_network_interface.nicApp.id]
+
+  os_disk {
+    name                 = "${local.prefixName}OsDisk-app"
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+  admin_ssh_key {
+    username   = "nabila"
+    public_key = file("~/.ssh/terraform_key.pub")
+  }
+}
