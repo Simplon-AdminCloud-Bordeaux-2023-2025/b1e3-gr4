f9c80d79c93cde58d203baa01d12949f84aaf91c
Nab244 <nrizki@simplonformations.onmicrosoft.com>
2023-07-04 16:26:48 +0200

    randompassdb

--- bdd.tf
+++ bdd.tf
@@ -1,13 +1,26 @@
+resource "random_password" "dbpass" {
+  length           = 16
+  special          = true
+  min_lower        = 4
+  min_upper        = 2
+  min_special      = 2
+  min_numeric      = 3
+  override_special = "!()-_=+[]{}<>:?"
+}
+
+
+
 resource "azurerm_mariadb_server" "dbserver" {
   name                = "${local.prefixName}mariadb-server"
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
 
   administrator_login          = "mariadbadmin"
-  administrator_login_password = "otarierouge0307!"
-  sku_name                     = "GP_Gen5_2"
-  storage_mb                   = 5120
-  version                      = "10.2"
+  administrator_login_password = random_password.dbpass.result
+
+  sku_name   = "GP_Gen5_2"
+  storage_mb = 5120
+  version    = "10.2"
 
   auto_grow_enabled                = true
   backup_retention_days            = 7
@@ -19,11 +32,19 @@ resource "azurerm_mariadb_server" "dbserver" {
 
 
 resource "azurerm_subnet" "dbsubnet" {
-  name                 = "internal"
+  name                 = "${local.prefixName}subnet-mariadb"
   resource_group_name  = data.azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.Vnet.name
   address_prefixes     = ["10.1.3.0/24"]
   service_endpoints    = ["Microsoft.Sql"]
+}
+
+resource "azurerm_mariadb_firewall_rule" "apprule" {
+  name                = "${local.prefixName}firewalldbrule"
+  resource_group_name = data.azurerm_resource_group.rg.name
+  server_name         = azurerm_mariadb_server.dbserver.name
+  start_ip_address    = azurerm_public_ip.ipApp.ip_address
+  end_ip_address      = azurerm_public_ip.ipApp.ip_address
 }
 
 resource "azurerm_mariadb_virtual_network_rule" "subnetasso" {
